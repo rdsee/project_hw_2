f912fefc04852a053c8ef3f419872fbdc960a669
rdsee <rost.slobod@gmail.com>
2026-03-04 12:11:37 +0200

    changed file, added condition theory

---
+++ practice.py
@@ -0,0 +1,21 @@
+n1, n2, n3 = 2, 3, 2
+
+print(n1 > n2)
+print(n1 < n2)
+print(n1 >= n2)
+print(n1 <= n2)
+print('\t')
+
+print(n1 == n2) #если н1 не равно н2 тогда выводит false
+print(n1 != n2) #если н1 не равно н2 тогда выводи true
+print('\t')
+
+print (n1 == n2 or n2 == n3) #будет вывод true, если н1 равно одному из двуз условий
+print(n1 == n3 and n1 != n2) #выводит true, если н1 равно одному и неравно другому одновременно
+print('\t')
+
+is_valid = True
+print(is_valid)
+print(not is_valid) #Это инверсия, когда получается изменить одно значение на другое, работает с true / false.
+
+print('a' in 'hello wold') #так как буквы а - нет, вывод будет False.
